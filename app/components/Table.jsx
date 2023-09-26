764514d9fe1dcdc103be3a43e1f711884de0232a
leandrology <leandrology@gmail.com>
2023-09-27 06:10:59 +0800

    added header & pagination

--- app/components/Table.jsx
+++ app/components/Table.jsx
@@ -46,6 +46,41 @@ export default function Table() {
     //     });
     // };
 
+    const customTotal = (from, to, size) => (
+        <span className="react-bootstrap-table-pagination-total">
+          Showing { from } to { to } of { size } Results
+        </span>
+      );
+      
+      const options = {
+        paginationSize: 4,
+        pageStartIndex: 0,
+        // alwaysShowAllBtns: true, // Always show next and previous button
+        // withFirstAndLast: false, // Hide the going to First and Last page button
+        // hideSizePerPage: true, // Hide the sizePerPage dropdown always
+        // hidePageListOnlyOnePage: true, // Hide the pagination list when only one page
+        firstPageText: 'First',
+        prePageText: 'Back',
+        nextPageText: 'Next',
+        lastPageText: 'Last',
+        nextPageTitle: 'First page',
+        prePageTitle: 'Pre page',
+        firstPageTitle: 'Next page',
+        lastPageTitle: 'Last page',
+        showTotal: true,
+        paginationTotalRenderer: customTotal,
+        disablePageTitle: true,
+        sizePerPageList: [{
+          text: '5', value: 5
+        }, {
+            text: '10', value: 10
+        }, {
+            text: '25', value: 25
+        }, {
+          text: 'All', value: products.length
+        }] // A numeric array is also available. the purpose of above example is custom the text
+      };
+
     const dayOptions = {
         Monday: 'Monday',
         Tuesday: 'Tuesday',
@@ -118,7 +153,8 @@ export default function Table() {
   
   return (
     <div className='container mx-auto py-8'>
-      <BootstrapTable keyField='id' data={products} columns={columns} striped hover condensed pagination={paginationFactory()} filter={filterFactory()}/>
+        <h1 className='py-4'>Francis Data Table</h1>
+      <BootstrapTable keyField='id' data={products} columns={columns} striped hover condensed pagination={paginationFactory(options)} filter={filterFactory()}/>
     </div>
   )
 }
